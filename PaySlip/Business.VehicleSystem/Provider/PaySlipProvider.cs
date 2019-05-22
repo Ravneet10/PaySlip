94d4030e15e221e853bbd0ac656e8142f526da7e
Ravneet10 <49365317+Ravneet10@users.noreply.github.com>
2019-05-22 11:22:01 +1000

    Implemented Single resposibility principle

--- PaySlip/Business.VehicleSystem/Provider/PaySlipProvider.cs
+++ PaySlip/Business.VehicleSystem/Provider/PaySlipProvider.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 using Business.PaySlip.Interface;
 using Business.PaySlip.Model;
+using Business.PaySlip.Domain;
 namespace Business.PaySlip.Provider
 {
-    public class PaySlipProvider : IPaySlip
+    public class PaySlipProvider : IncomeTaxCalc, IPaySlip
     {
         public EmployeePaySlipDetails GeneratePaySlip(EmployeeDetails employeeDetails)
         {
+
             double grossIncome = employeeDetails.AnnualSalary / 12;
             RoundData(ref grossIncome);
             double incomeTax = CalculateIncomeTax(Convert.ToDouble(employeeDetails.AnnualSalary));
@@ -32,30 +34,17 @@ namespace Business.PaySlip.Provider
         }
         public void RoundData(ref double data)
         {
-            data = data <= 0.50 ? Math.Floor(data) : Math.Round(data, MidpointRounding.AwayFromZero);
+            if (data > 0)
+            {
+                data = data <= 0.50 ? Math.Floor(data) : Math.Round(data, MidpointRounding.AwayFromZero);
+
+            }
+            else
+            {
+                throw new ArgumentException("Invalid format");
+            }
 
         }
-        private double CalculateIncomeTax(double AnnaulPackage)
-        {
-            if (AnnaulPackage < 18200)
-                return 0;
 
-            else if (AnnaulPackage > 18200 && AnnaulPackage <= 37000)
-            {
-                return (0.19 * (AnnaulPackage - 18200)) / 12;
-            }
-
-            else if (AnnaulPackage > 37000 && AnnaulPackage <= 87000)
-            {
-                return (3572 + (0.325 * (AnnaulPackage - 37000))) / 12;
-            }
-
-            else if (AnnaulPackage > 87000 && AnnaulPackage <= 180000)
-            {
-                return (19882 + (0.37 * (AnnaulPackage - 87000))) / 12;
-            }
-
-            return (54232 + (0.45 * (AnnaulPackage - 180000))) / 12;
-        }
     }
 }
