561e15ff6b35d2cc34dba3fa21ec7037a053074f
Ravneet10 <49365317+Ravneet10@users.noreply.github.com>
2019-05-23 12:00:27 +1000

    Modified structure of the class to follow singlr responsibility rule.

--- PaySlip/Business.VehicleSystem/Domain/IncomeTaxCalc.cs
+++ PaySlip/Business.VehicleSystem/Domain/IncomeTaxCalc.cs
@@ -6,29 +6,63 @@ using Business.PaySlip.Constants;
 
 namespace Business.PaySlip.Domain
 {
+    //cyclomatic complexity
+    /// <summary>
+    /// calculate common logic for income tax calculation.
+    /// </summary>
    public abstract class IncomeTaxCalc
     {
-        public double CalculateIncomeTax(double AnnaulPackage)
+        public virtual double CalculateIncomeTax(double AnnualPackage)
         {
-            if (AnnaulPackage < Constant.AnnualPackageConstant)
+            if (AnnualPackage < Constant.AnnualPackageConstant)
                 return 0;
 
-            else if (AnnaulPackage > Constant.AnnualPackageConstant && AnnaulPackage <= Constant.AnnualPackageSecondConstant)
+            else if (AnnualPackage > Constant.AnnualPackageConstant && AnnualPackage <= Constant.AnnualPackageSecondConstant)
             {
-                return (0.19 * (AnnaulPackage - Constant.AnnualPackageConstant)) / 12;
+                return (0.19 * (AnnualPackage - Constant.AnnualPackageConstant)) / 12;
             }
 
-            else if (AnnaulPackage > Constant.AnnualPackageSecondConstant && AnnaulPackage <= 87000)
+            else if (AnnualPackage > Constant.AnnualPackageSecondConstant && AnnualPackage <= 87000)
             {
-                return (3572 + (0.325 * (AnnaulPackage - Constant.AnnualPackageSecondConstant))) / 12;
+                return (3572 + (0.325 * (AnnualPackage - Constant.AnnualPackageSecondConstant))) / 12;
             }
 
-            else if (AnnaulPackage > 87000 && AnnaulPackage <= 180000)
+            else if (AnnualPackage > 87000 && AnnualPackage <= 180000)
             {
-                return (19882 + (0.37 * (AnnaulPackage - 87000))) / 12;
+                return (19882 + (0.37 * (AnnualPackage - 87000))) / 12;
             }
 
-            return (54232 + (0.45 * (AnnaulPackage - 180000))) / 12;
+            return (54232 + (0.45 * (AnnualPackage - 180000))) / 12;
+        }
+        public  double CalculateGrossIncome( double AnnualSalary)
+        {
+            double grossIncome = AnnualSalary / 12;
+           return RoundData(grossIncome);
+        }
+        public  double CalculateNetIncome(double grossIncome,double incomeTax)
+        {
+            double netIncome = grossIncome - incomeTax;
+            return RoundData(netIncome);
+        }
+        public  double CalculateSuper(double grossIncome,double superRate)
+        {
+            double super = (grossIncome * superRate) / 100;
+            return RoundData(super);
+        }
+      
+        public virtual double RoundData(double data)
+        {
+            if (data > 0)
+            {
+                data = data <= 0.50 ? Math.Floor(data) : Math.Round(data, MidpointRounding.AwayFromZero);
+
+            }
+            else
+            {
+                throw new ArgumentException("Invalid format");
+            }
+            return data;
+
         }
     }
 }
